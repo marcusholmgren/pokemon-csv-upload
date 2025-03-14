13494a00fc03c69f2fef373bbfd7357448fbe21f
Marcus Holmgren <marcus.holmgren1@gmail.com>
2025-03-14 07:02:05 +0100

    Read Pokemon csv data
    - CSV data type
    - DB data type

--- crates/upload-pokemon-data/src/main.rs
+++ crates/upload-pokemon-data/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+mod db;
+mod pokemon_csv;
+use db::*;
+use pokemon_csv::*;
+
+fn main() -> Result<(), csv::Error> {
+    let mut rdr = csv::Reader::from_path("./crates/upload-pokemon-data/pokemon.csv")?;
+    for result in rdr.deserialize() {
+        let record: PokemonCsv = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
 }
